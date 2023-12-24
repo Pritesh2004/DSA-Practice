a626fd7553fdfd9db1517a7f768f7c649917c1b0
Pritesh2004 <pmpatilpritesh123@gmail.com>
2023-12-24 13:49:41 +0530

    BinarySearch2dArray

---
+++ practice/BinarySearchIn2DArray.java
@@ -0,0 +1,39 @@
+package practice;
+
+import java.util.Arrays;
+
+public class BinarySearchIn2DArray {
+
+    public static void main(String[] args) {
+
+        int[][] nums ={
+                {10,20,30,40},
+                {15,25,35,45},
+                {28,29,37,49},
+                {33,34,38,50}
+        };
+        int target = 50;
+
+        System.out.println(Arrays.toString(search(nums,target)));
+    }
+
+    public static int[] search(int[][] nums, int target){
+
+        int row = 0;
+        int col = nums.length -1;
+
+        while(row < nums.length && col >= 0){
+
+            if(nums[row][col] == target){
+                return new int[]{row, col};
+            }
+            else if(nums[row][col] < target){
+                row++;
+            }
+            else {
+                col--;
+            }
+        }
+        return new int[]{-1,-1};
+    }
+}
---
+++ practice/BsInSortedMatrix.java
@@ -0,0 +1,88 @@
+package practice;
+
+import java.util.Arrays;
+
+public class BsInSortedMatrix {
+
+    public static void main(String[] args) {
+
+        int[][] nums={
+                {1,2,3},
+                {4,5,6},
+                {7,8,9}
+        };
+        int target = 8;
+
+        System.out.println(Arrays.toString(search(nums,target)));
+
+    }
+
+    public static int[] binarySearch(int[][] nums,int row,int cStart,int cEnd, int target){
+
+        while (cStart <= cEnd){
+
+            int mid = cStart + (cEnd - cStart)/2;
+
+            if(nums[row][mid] == target){
+                return new int[]{row,mid};
+            }
+            else if (nums[row][mid] > target) {
+                cEnd = mid -1;
+            }
+            else {
+                cStart = mid +1;
+            }
+        }
+        return new int[]{-1,-1};
+    }
+
+    public static int[] search(int[][] nums, int target){
+
+        int rows = nums.length;
+        int cols = nums[0].length;
+
+        if(rows == 1){
+            return binarySearch(nums,0,0,cols-1,target);
+        }
+
+        int rStart = 0;
+        int rEnd = rows -1;
+        int cMid = cols/2;
+
+        while(rStart < (rEnd -1)){
+
+            int mid = rStart + (rEnd - rStart)/2;
+            if(nums[mid][cMid]==target){
+                return new int[]{mid,cMid};
+            }
+            else if(nums[mid][cMid] < target){
+                rStart = mid;
+            }
+            else {
+                rEnd = mid;
+            }
+
+            if(nums[rStart][cMid]==target){
+                return new int[]{rStart,cMid};
+            }
+            if(nums[rStart+1][cMid]==target){
+                return new int[]{rStart+1,cMid};
+            }
+
+
+            if(target <= nums[rStart][cMid-1]){
+                return binarySearch(nums,rStart,0,cMid-1,target);
+            }
+            if(target >= nums[rStart][cMid+1] && target <= nums[rStart][cols -1]){
+                return binarySearch(nums,rStart,cMid+1,cols-1,target);
+            }
+            if(target <= nums[rStart+1][cMid-1]){
+                return binarySearch(nums,rStart+1,0,cMid-1,target);
+            }
+            else{
+                return binarySearch(nums,rStart+1,cMid+1,cols-1,target);
+            }
+        }
+        return new int[]{-1,-1};
+    }
+}
